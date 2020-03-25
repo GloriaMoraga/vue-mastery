7c719aaf90c2a33f2dced54df0567e452b099dad
GloriaMoraga <gloria.moraga.morales@gmail.com>
2020-03-25 09:14:23 -0300

    adding the image and put de V-bind mode

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var app = new Vue ({
     el: '#app',
     data : {
-        product: "socks"
+        product: 'socks',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        link:'http://www.google.cl'
     }
 
 
