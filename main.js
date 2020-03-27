cc4704542a619370c44c16965d330b7d24556256
GloriaMoraga <gloria.moraga.morales@gmail.com>
2020-03-27 17:32:43 -0300

    lesson 3/4 vuemastery

--- main.js
+++ main.js
@@ -3,7 +3,20 @@ var app = new Vue ({
     data : {
         product: 'socks',
         image: './assets/vmSocks-green-onWhite.jpg',
-        link:'http://www.google.cl'
+        link:'http://www.google.cl',
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+          {
+            variantId: 2234,
+            variantColor: 'green'    
+          },
+          {
+            variantId: 2235,
+            variantColor: 'blue'
+          }
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL']
     }
 
 
